564d8b048e448d58ae90e36820b13065d371e07b
ᎬᏁᎶᎥᏁᎬᎬᏒ <he-dev@users.noreply.github.com>
2026-05-04 19:45:10 +0200

    Removes everything

--- Wiretap.Tests/src/Fluorite/Helper.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Reusable.Tests.MarkupBuilder
-{
-    internal class Helper
-    {
-        public static IResourceProvider ResourceProvider { get; } = new RelativeProvider(new EmbeddedFileProvider(typeof(Helper).Assembly), @"res\MarkupBuilder");
-    }
-}
