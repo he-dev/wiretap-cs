5df13aebb2130496c04b6d391ffa3bebc873b94a
ᎬᏁᎶᎥᏁᎬᎬᏒ <he-dev@users.noreply.github.com>
2026-05-04 19:45:33 +0200

    Deletes everything

--- Wiretap.AspNetCore/src/Abstractions/IFilter.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Http;
-
-namespace Reusable.Wiretap.AspNetCore.Abstractions;
-
-public interface IFilter<T>
-{
-    bool Matches(HttpContext context);
-}
--- Wiretap.AspNetCore/src/Abstractions/ITakeSnapshot.cs
+++
@@ -1,6 +0,0 @@
-namespace Reusable.Wiretap.AspNetCore.Abstractions;
-
-public interface ITakeSnapshot<in T>
-{
-    object Invoke(T entity);
-}
